fe1c122a74ea3590f633ecd4ff08023cc66ad613
Huy <s3825059@rmit.edu.vn>
2022-11-02 22:54:42 +0700

    change bootstrap.css to bootstrap.min.css to avoid error when deploy

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "../assets/nasa-logo.png";
 
 const Header = () => {
